d115f960b970a76a069aad95e767005bf4cb608b
Garinoth <cdavidrp@gmail.com>
2012-11-15 19:42:23 +0100

    Almost completed SceneSetup (may need a skybox change and some tweaking)

--- js/three/SceneSetup.js
+++ js/three/SceneSetup.js
@@ -1,37 +1,59 @@
 
-var camera, scene, renderer, controls;
+var camera, scene, renderer;
 
 init();
 animate();
 
 function init() {
 	
-    renderer = new THREE.WebGLRenderer({antialias: true});
-    renderer.setSize( window.innerWidth -15, window.innerHeight -21 );
-    
+	canvas = $('canvas').get(0);
+	
+    renderer = new THREE.WebGLRenderer({ antialias: true, canvas : canvas });
+    renderer.setSize( canvas.width, canvas.height );
+        
     scene = new THREE.Scene();
 
-    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
-    camera.position.z = 500;
+    camera = new THREE.PerspectiveCamera( 45, canvas.width / canvas.height, 0.1, 10000 );
+    camera.position.z = 200;
+    
+    var urlPrefix	= "../../assets/textures/OrangeCreamSky";
+	var urls = [ urlPrefix + "/1.jpg", urlPrefix + "/2.jpg",
+			urlPrefix + "/5.jpg", urlPrefix + "/6.jpg",
+			urlPrefix + "/3.jpg", urlPrefix + "/4.jpg" ];
+			
+	var textureCube	= THREE.ImageUtils.loadTextureCube( urls );
 
-    $('body').append( renderer.domElement );
+	var shader	= THREE.ShaderUtils.lib["cube"];
+	shader.uniforms["tCube"].value = textureCube;
+	var material = new THREE.ShaderMaterial({
+		fragmentShader	: shader.fragmentShader,
+		vertexShader	: shader.vertexShader,
+		uniforms	: shader.uniforms,
+		side: THREE.BackSide
+	});
 
-    setControls (camera);
+	skyboxMesh	= new THREE.Mesh( new THREE.CubeGeometry( 1000, 1000, 1000 ), material );
+
+	scene.add( skyboxMesh );
+	
+    
+    var light = new THREE.PointLight( 0xffffff );
+	light.position.set( 250, 250, 250 );
+	scene.add(light);
+    
+    var geometry = new THREE.SphereGeometry( 50, 50, 50 );
+    var material = new THREE.MeshLambertMaterial();
+    var mesh = new THREE.Mesh( geometry, material );
+    scene.add( mesh );
 
 }
 
 function animate() {
     requestAnimationFrame( animate );
-    controls.update();
+    render();
 }
 
 
 function render () {
     renderer.render(scene, camera);
-}
-
-
-function setControls (camera){
-    controls = new THREE.TrackballControls( camera ); // Creates the controls
-    controls.addEventListener( 'change', render );    // Adds a listener for them to work
 }
