ac8582293b10eac06b240555189bf2b123cbb7d3
Inmakia <inmakia@gmail.com>
2012-11-16 11:15:19 +0100

    Added model import (json)

---
+++ js/three/ModelImportJSON.js
@@ -0,0 +1,65 @@
+
+var camera, scene, renderer;
+
+init();
+animate();
+
+function init() {
+	
+	canvas = $('canvas').get(0);
+	
+    renderer = new THREE.WebGLRenderer({ antialias: true, canvas : canvas });
+    renderer.setSize( canvas.width, canvas.height );
+        
+    scene = new THREE.Scene();
+
+    camera = new THREE.PerspectiveCamera( 45, canvas.width / canvas.height, 0.1, 10000 );
+    camera.position.z = 200;
+    
+    var urlPrefix	= "../../assets/textures/OrangeCreamSky";
+	var urls = [ urlPrefix + "/1.jpg", urlPrefix + "/2.jpg",
+			urlPrefix + "/5.jpg", urlPrefix + "/6.jpg",
+			urlPrefix + "/3.jpg", urlPrefix + "/4.jpg" ];
+			
+	var textureCube	= THREE.ImageUtils.loadTextureCube( urls );
+
+	var shader	= THREE.ShaderUtils.lib["cube"];
+	shader.uniforms["tCube"].value = textureCube;
+	var material = new THREE.ShaderMaterial({
+		fragmentShader	: shader.fragmentShader,
+		vertexShader	: shader.vertexShader,
+		uniforms	: shader.uniforms,
+		side: THREE.BackSide
+	});
+
+	skyboxMesh	= new THREE.Mesh( new THREE.CubeGeometry( 1000, 1000, 1000 ), material );
+
+	scene.add( skyboxMesh );
+	
+    
+    var light = new THREE.PointLight( 0xffffff );
+	light.position.set( 250, 250, 250 );
+	scene.add(light);
+	
+	var loader = new THREE.JSONLoader();
+	loader.load( "../../assets/models/Wolf.js", onGeometry );
+
+}
+
+function onGeometry (geom) {
+	
+	wolf = new THREE.Mesh( geom, new THREE.MeshLambertMaterial());
+	wolf.overdraw = true;
+	scene.add(wolf);
+}
+
+
+function animate() {
+    requestAnimationFrame( animate );
+    render();
+}
+
+
+function render () {
+    renderer.render(scene, camera);
+}
