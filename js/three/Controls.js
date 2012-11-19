899068d4da8ad97d2f020afeb9f4ccb5146fbbfe
Garinoth <cdavidrp@gmail.com>
2012-11-19 09:12:40 +0100

    Added model controls

--- js/three/Controls.js
+++ js/three/Controls.js
@@ -1,5 +1,25 @@
 
 var camera, scene, renderer;
+var speed = 5;
+var rotSpeed = 0.05;
+var m,r;
+var model;
+
+var config = {
+	SPEED : 5,
+	ROTSPEED : 0.05
+}
+
+var keyCodes = {
+      LEFT_ARROW: 37,
+      UP_ARROW: 38,
+      RIGHT_ARROW: 39,
+      DOWN_ARROW: 40,
+      KEY_A: 65,
+      KEY_D: 68,
+      KEY_S: 83,
+      KEY_W: 87,
+     }
 
 init();
 animate();
@@ -47,31 +67,94 @@ function init() {
 	scene.add(light);
     
     // Object
-    var geometry = new THREE.SphereGeometry( 50, 50, 50 );
+    var geometry = new THREE.CubeGeometry( 50, 50, 50 );
     var material = new THREE.MeshLambertMaterial();
-    var mesh = new THREE.Mesh( geometry, material );
-    scene.add( mesh );
+    model = new THREE.Mesh( geometry, material );
+    scene.add( model );
     
         
     // Camera controls
-    setControls (camera);
+    setCameraControls ( camera );
+    
+    
+    // Model controls
+    setModelControls ();
 
 }
 
 function animate() {
-    requestAnimationFrame( animate );
+	requestAnimationFrame( animate );
     controls.update();
+    
+    if (m) {
+    	model.translateZ(speed);
+    }
+    if (r) {
+    	model.rotation.y += rotSpeed;
+    }
+    
     render();
+    
 }
 
 
 function render () {
-    renderer.render(scene, camera);
+    renderer.render( scene, camera );
 }
 
 
-function setControls (camera){
+function setCameraControls ( camera ){
     controls = new THREE.TrackballControls( camera ); // Creates the controls
     controls.addEventListener( 'change', render );    // Adds a listener for them to work
     controls.maxDistance = 50000;
+}
+
+
+function move( event ) {
+	
+	if ( (event.keyCode == keyCodes.UP_ARROW || event.keyCode == keyCodes.KEY_W) && !m ) {
+		speed = config.SPEED;
+		m = true;
+	}
+	
+	if ( (event.keyCode == keyCodes.DOWN_ARROW || event.keyCode == keyCodes.KEY_S) && !m ) {
+		speed = -config.SPEED;
+		m = true;
+	}
+	
+	if ( (event.keyCode == keyCodes.RIGHT_ARROW || event.keyCode == keyCodes.KEY_D) && !r ) {
+		rotSpeed = -config.ROTSPEED;
+		r = true;
+	}
+	
+	if ( (event.keyCode == keyCodes.LEFT_ARROW || event.keyCode == keyCodes.KEY_A) && !r ) {
+		rotSpeed = config.ROTSPEED;
+		r = true;
+	}
+}
+
+
+function stop( event ) {
+	
+	if ( (event.keyCode == keyCodes.UP_ARROW || event.keyCode == keyCodes.KEY_W) && m ) {
+		m = false;
+	}
+	
+	if ( (event.keyCode == keyCodes.DOWN_ARROW || event.keyCode == keyCodes.KEY_S) && m ) {
+		m = false;
+	}
+	
+	if ( (event.keyCode == keyCodes.RIGHT_ARROW || event.keyCode == keyCodes.KEY_D) && r ) {
+		r = false;
+	}
+	
+	if ( (event.keyCode == keyCodes.LEFT_ARROW || event.keyCode == keyCodes.KEY_A) && r ) {
+		r = false;
+	}
+}
+
+
+function setModelControls() {
+	$(window).on('keydown', move);
+	$(window).on('keyup', stop);
 }
