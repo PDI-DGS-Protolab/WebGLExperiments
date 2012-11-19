3d1fb5fe293e3c98692f7b14f5b85ca01dd6bcdb
albertoblaz <hola@albertoblazquez.net>
2012-11-19 11:41:29 +0100

    Car controls improved and aplane texture uploaded

--- js/cubic/controls.js
+++ js/cubic/controls.js
@@ -13,102 +13,109 @@
             light : {
                 name     : "light",
                 type     : "point",
-                position : [1.0, 1.5, -2.0]
+                position : [0.0, 2.0, 0.0]
             },
 
             camera : {
                 name     : "camera",
                 width    : canvas.width,
                 height   : canvas.height,
-                position : [1.0, 1.0, 1.0],
+                position : [0.0, 0.5, -2.0],
                 target   : [0, 0, 0],
                 fov      : 60.0
             },
 
-            sceneObjects : []
+            sceneObjects : [
+                {
+                    name : "plane",
+                    position : [0.0, 0.0, 0.0],
+                    rotation : [90, 0, 0],
+                    mesh: {
+                        primitive: {
+                            type: "plane",
+                            size: 10.0,
+                            material: {
+                                textures: {
+                                    color: "assets/textures/plane.jpg"
+                                }
+                            },
+                            uv: {
+                                projectionMode: "planar"
+                                //scale: [0.015, 0.015, 0.015]
+                            }
+                        },
+                        compile: true
+                    }
+                }
+            ]
 
         });
 
 
         // Collada model imported
-
+/*
         var colladaScene = CubicVR.loadCollada("assets/duck/duck.dae", "assets/duck/");
         var duckMesh = colladaScene.getSceneObject("LOD3sp").obj;           // need to know it's name in the default scene
         var duck = new CubicVR.SceneObject({                            // SceneObject container for the mesh
             name : 'duck',
             mesh : duckMesh,
-            position: [ 0.0, 0.0, 0.0 ],
+            position: [ 0.0, -0.05, 0.0 ],
             scale : [ 0.005, 0.005, 0.005 ]
         });
         scene.bindSceneObject(duck);
-
-/*
-        var wolfMesh = CubicVR.get("assets/duck/duck.json", CubicVR.Mesh);
-        var wolf = new CubicVR.SceneObject({
-            mesh     : wolfMesh,
-            position : [0.0, 0.0, -2.0],
-            //scale    : [10.0, 10.0, 10.0]
-            scale : [ 0.005, 0.005, 0.005 ]
-        });
-
-        scene.bindSceneObject(wolf);
 */
+
+        var colladaScene = CubicVR.loadCollada("assets/models/sportscar/car1.dae", "assets/models/sportscar/");
+        var carMesh = colladaScene.getSceneObject("car1").obj;           // need to know it's name in the default scene
+        var car = new CubicVR.SceneObject({                            // SceneObject container for the mesh
+            name : 'car',
+            mesh : carMesh,
+            position: [ 0.0, 0.015, 0.0 ],
+            scale : [ 0.1, 0.1, 0.1 ]
+        });
+        scene.bindSceneObject(car);
+
 
         // Camera resizable with enabled controls
         CubicVR.addResizeable(scene.camera);
 
+        var rot = 0;
         var moving  = 0;
         var turning = 0;
         var kbd = CubicVR.keyboard;
 
         var mvc = new CubicVR.MouseViewController(canvas, scene.camera);
-        mvc.setEvents({
 
-            keyDown : function(ctx, mpos, keyCode, keyState) {
+        mvc.bindEvent('keyDown', function(ctx, mpos, keyCode, keyState) {
 
-                if (keyCode === kbd.UP_ARROW) {
-                    moving = 1;
+            if (keyCode === kbd.UP_ARROW) {
+                moving = 1;
 
-                } else if (keyCode === kbd.DOWN_ARROW) {
-                    moving = -1;
-                }
-
-                if (keyCode === kbd.LEFT_ARROW) {
-                    turning = 1;
-
-                } else if (keyCode === kbd.RIGHT_ARROW) {
-                    turning = -1;
-                }
-
-                return true;
-            },
-
-            keyUp : function(ctx, mpos, keyCode, keyState) {
-
-                if (keyCode === kbd.UP_ARROW || keyCode === kbd.DOWN_ARROW) {
-                    moving = 0;
-
-                } else if (keyCode === kbd.LEFT_ARROW || keyCode === kbd.RIGHT_ARROW) {
-                    turning = 0;
-                }
-
-                return true;
-            },
-
-            mouseMove: function (ctx, mpos, mdelta, keyState) {
-                if (!ctx.mdown) return;
-
-                ctx.orbitView(mdelta);
-                //          ctx.panView(mdelta);
-            },
-
-            mouseWheel: function (ctx, mpos, wdelta, keyState) {
-                ctx.zoomView(wdelta);
+            } else if (keyCode === kbd.DOWN_ARROW) {
+                moving = -1;
             }
 
+            if (keyCode === kbd.LEFT_ARROW) {
+                turning = 1;
+
+            } else if (keyCode === kbd.RIGHT_ARROW) {
+                turning = -1;
+            }
+
+            //return true;
         });
 
+        mvc.bindEvent('keyUp', function(ctx, mpos, keyCode, keyState) {
 
+            if (keyCode === kbd.UP_ARROW || keyCode === kbd.DOWN_ARROW) {
+                moving = 0;
+
+            } else if (keyCode === kbd.LEFT_ARROW || keyCode === kbd.RIGHT_ARROW) {
+                turning = 0;
+            }
+
+            //return true;
+        });
 
 
         scene.setSkyBox(new CubicVR.SkyBox({
@@ -117,15 +124,25 @@
 
 
         CubicVR.MainLoop(function(timer, gl) {
+
+            var car = scene.getSceneObject('car');
+
             scene.render();
 
             if (moving) {
-                scene.getSceneObject('duck').x += moving / 100;
+                var move  = moving / 30;
+                var angle = rot * Math.PI / 180;
+                car.x += move * Math.sin(angle);
+                car.z += move * Math.cos(angle);
             }
 
             if (turning) {
-                scene.getSceneObject('duck').z -= turning / 100;
+                rot += turning;
+                rot %= 360;
+                car.rotY += turning;
             }
+
+            scene.camera.target = [ car.x, car.y, car.z ];
 
         });
 
