69c884082dd9dd3c83ddaeef431dfc2face04c17
Inmakia <inmakia@gmail.com>
2012-11-20 13:41:15 +0100

    Added import for collada models

--- js/three/ModelImportOthers.js
+++ js/three/ModelImportOthers.js
@@ -44,7 +44,7 @@ function init() {
 	
 	
     // Light
-    var light = new THREE.PointLight( 0xffffff );
+    var light = new THREE.DirectionalLight( 0xffffff );
 	light.position.set( -250, 250, -250 );
 	scene.add(light);
     
@@ -52,34 +52,12 @@ function init() {
     // Camera controls
     setControls (camera);
     
-    // Model import
-    var loader = new THREE.JSONLoader();	
-	
-	tex = THREE.ImageUtils.loadTexture('../../assets/textures/Wolf_Diffuse_256x256.jpg', null, function () {
-            mat = new THREE.MeshBasicMaterial({ map: tex, morphTargets: true });
-            loader.load('../../assets/models/Wolf.js', function (geo) {
-                wolf = new THREE.Mesh(geo, mat);
-                wolf.scale.set(20, 20, 20);
-				wolf.position.set(0, -500, 0);
-                // etc, etc
-                scene.add(wolf);
-            });
-        });      
-}
 
 	// Model import others
-	var loader = new THREE.ColladaLoader();
-	loader.options.convertUpAxis = true;
-	loader.load( '../../assets/models/duck/duck.dae',function colladaReady( collada ) {
-	
-	duck = collada.scene;
-	geo = collada.scene.children[ 0 ].geometry;
-	mat = collada.scene.children[ 0 ].material;
-	
-	duck.scale.set(10.0, 10.0, 10.0);
-	duck.updateMatrix();
-	scene.add(duck);
-	});
+	model = importModel();
+
+}
+
 
 function animate() {
     requestAnimationFrame( animate );
@@ -97,4 +75,19 @@ function setControls (camera){
     controls = new THREE.TrackballControls( camera ); // Creates the controls
     controls.addEventListener( 'change', render );    // Adds a listener for them to work
     controls.maxDistance = 50000;
+}
+
+function importModel(){
+	var loader = new THREE.ColladaLoader();
+	loader.options.convertUpAxis = true;
+	loader.load( '../../assets/models/duck/duck.dae',function colladaReady( collada ) {
+	
+	duck = collada.scene;
+	geo = collada.scene.children[ 0 ].geometry;
+	mat = collada.scene.children[ 0 ].material;
+	
+	duck.scale.set(5, 5, 5);
+	duck.updateMatrix();
+	scene.add(duck);
+	});
 }
