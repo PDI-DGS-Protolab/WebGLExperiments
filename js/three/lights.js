7cd9886d6984bf61b73990388c8fe4133eb793f2
albertoblaz <hola@albertoblazquez.net>
2012-11-30 11:58:58 +0100

    Fixed some little bugs. It is now perfectly integrated with the Visualizator

--- js/three/lights.js
+++ js/three/lights.js
@@ -1,53 +1,36 @@
 
 var camera, scene, renderer;
 var clock = new THREE.Clock();
-var speed = 5;
-var rotSpeed = 0.05;
-var m,r;
+var rotSpeed = 0.015;
 var model;
 var lookAtModel = false;
 
-var config = {
-	SPEED : 5,
-	ROTSPEED : 0.05
-}
-
-var keyCodes = {
-      LEFT_ARROW: 37,
-      UP_ARROW: 38,
-      RIGHT_ARROW: 39,
-      DOWN_ARROW: 40,
-      KEY_A: 65,
-      KEY_D: 68,
-      KEY_S: 83,
-      KEY_W: 87,
-     }
 
 init();
 animate();
 
 function init() {
-	
+
 	// Renderer
     renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
 
-    
+
     // Scene
     scene = new THREE.Scene();
-	
+
 	// Camera
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 200000 ); 
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 200000 );
     camera.position.z = -500;
     camera.position.y = 200;
-    
+
     // Skybox
     var urlPrefix	= "../assets/textures";
 	var urls = [ urlPrefix + "/skybox1.jpg", urlPrefix + "/skybox3.jpg",
 			urlPrefix + "/skybox5.jpg", urlPrefix + "/skybox4.jpg",
 			urlPrefix + "/skybox2.jpg", urlPrefix + "/skybox6.jpg" ];
-			
+
 	var textureCube	= THREE.ImageUtils.loadTextureCube( urls );
 
 	var shader	= THREE.ShaderUtils.lib["cube"];
@@ -62,20 +45,17 @@ function init() {
 	skyboxMesh	= new THREE.Mesh( new THREE.CubeGeometry( 100000, 100000, 100000, 1, 1, 1, null, true ), material );
 
 	scene.add( skyboxMesh );
-	
-	
+
+
     // Light
     addLights();
-    
+
     // Camera controls
     setCameraControls( camera );
-    
-    // Model controls
-    setModelControls();
-    
+
 	// Camera follow
- 	lookAtModel = true;
- 
+    lookAtModel = true;
+
 	// Camera chase
 
 	// Model import others
@@ -87,19 +67,13 @@ function init() {
 function animate() {
 	var time = Date.now() * 0.0005;
 	var delta = clock.getDelta();
-	
+
 	requestAnimationFrame( animate );
     controls.update();
-    
-    if (m) {
-    	model.translateX(-speed);
-    }
-    if (r) {
-    	model.rotation.y += rotSpeed;
-    }
-    
+
     if (lookAtModel && model) {
-    	camera.lookAt(model.position);
+        camera.lookAt(model.position);
+        model.rotation.y += rotSpeed;
     }
 
 	light1.position.x = Math.sin( time * 0.7 ) * 200;
@@ -117,9 +91,9 @@ function animate() {
 	light4.position.x = Math.sin( time * 0.3 ) * 200;
 	light4.position.y = Math.cos( time * 0.7 ) * 250;
 	light4.position.z = Math.sin( time * 0.5 ) * 200;
-	
+
     render();
-    
+
 }
 
 
@@ -170,55 +144,6 @@ function addLights (){
 	scene.add( l4 );
 }
 
-
-function move( event ) {
-	
-	if ( (event.keyCode == keyCodes.UP_ARROW || event.keyCode == keyCodes.KEY_W) && !m ) {
-		speed = -config.SPEED;
-		m = true;
-	}
-	
-	if ( (event.keyCode == keyCodes.DOWN_ARROW || event.keyCode == keyCodes.KEY_S) && !m ) {
-		speed = config.SPEED;
-		m = true;
-	}
-	
-	if ( (event.keyCode == keyCodes.RIGHT_ARROW || event.keyCode == keyCodes.KEY_D) && !r ) {
-		rotSpeed = -config.ROTSPEED;
-		r = true;
-	}
-	
-	if ( (event.keyCode == keyCodes.LEFT_ARROW || event.keyCode == keyCodes.KEY_A) && !r ) {
-		rotSpeed = config.ROTSPEED;
-		r = true;
-	}
-}
-
-
-function stop( event ) {
-	
-	if ( (event.keyCode == keyCodes.UP_ARROW || event.keyCode == keyCodes.KEY_W) && m ) {
-		m = false;
-	}
-	
-	if ( (event.keyCode == keyCodes.DOWN_ARROW || event.keyCode == keyCodes.KEY_S) && m ) {
-		m = false;
-	}
-	
-	if ( (event.keyCode == keyCodes.RIGHT_ARROW || event.keyCode == keyCodes.KEY_D) && r ) {
-		r = false;
-	}
-	
-	if ( (event.keyCode == keyCodes.LEFT_ARROW || event.keyCode == keyCodes.KEY_A) && r ) {
-		r = false;
-	}
-}
-
-
-function setModelControls() {
-	$(window).on('keydown', move);
-	$(window).on('keyup', stop);
-}
 
 function importModelCollada(){
 	var loader = new THREE.ColladaLoader();
--- js/three/models-json.js
+++ js/three/models-json.js
@@ -1,51 +1,33 @@
 
 var camera, scene, renderer;
-var speed = 5;
-var rotSpeed = 0.05;
-var m,r;
 var model;
 var lookAtModel = false;
-
-var config = {
-	SPEED : 5,
-	ROTSPEED : 0.05
-}
-
-var keyCodes = {
-      LEFT_ARROW: 37,
-      UP_ARROW: 38,
-      RIGHT_ARROW: 39,
-      DOWN_ARROW: 40,
-      KEY_A: 65,
-      KEY_D: 68,
-      KEY_S: 83,
-      KEY_W: 87,
-     }
 
 init();
 animate();
 
 function init() {
-	
+
 	// Renderer
     renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
 
-    
+
     // Scene
     scene = new THREE.Scene();
-	
+
 	// Camera
     camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 200000 );
-    camera.position.z = -2000;
-    
+    camera.position.y = 30;
+    camera.position.z = -100;
+
     // Skybox
     var urlPrefix	= "../assets/textures";
 	var urls = [ urlPrefix + "/skybox1.jpg", urlPrefix + "/skybox3.jpg",
 			urlPrefix + "/skybox5.jpg", urlPrefix + "/skybox4.jpg",
 			urlPrefix + "/skybox2.jpg", urlPrefix + "/skybox6.jpg" ];
-			
+
 	var textureCube	= THREE.ImageUtils.loadTextureCube( urls );
 
 	var shader	= THREE.ShaderUtils.lib["cube"];
@@ -60,26 +42,23 @@ function init() {
 	skyboxMesh	= new THREE.Mesh( new THREE.CubeGeometry( 100000, 100000, 100000, 1, 1, 1, null, true ), material );
 
 	scene.add( skyboxMesh );
-	
+
     // Light
     var light = new THREE.DirectionalLight( 0xffffff );
 	light.position.set( -250, 250, -250 );
 	scene.add(light);
-    
-        
+
+
     // Camera controls
     setCameraControls( camera );
-    
-    // Model controls
-    setModelControls();
-    
+
 	// Camera follow
- 	lookAtModel = true;
- 
+    lookAtModel = true;
+
 	// Camera chase
-    
+
     // Model import (JSON)
-	importModelJSON();     
+	importModelJSON();
 
 }
 
@@ -87,20 +66,13 @@ function init() {
 function animate() {
 	requestAnimationFrame( animate );
     controls.update();
-    
-    if (m) {
-    	model.translateZ(speed);
-    }
-    if (r) {
-    	model.rotation.y += rotSpeed;
-    }
-    
+
     if (lookAtModel && model) {
-    	camera.lookAt(model.position);
+        camera.lookAt(model.position);
     }
-    
+
     render();
-    
+
 }
 
 
@@ -115,63 +87,15 @@ function setCameraControls ( camera ){
     controls.maxDistance = 50000;
 }
 
-function move( event ) {
-	
-	if ( (event.keyCode == keyCodes.UP_ARROW || event.keyCode == keyCodes.KEY_W) && !m ) {
-		speed = -config.SPEED;
-		m = true;
-	}
-	
-	if ( (event.keyCode == keyCodes.DOWN_ARROW || event.keyCode == keyCodes.KEY_S) && !m ) {
-		speed = config.SPEED;
-		m = true;
-	}
-	
-	if ( (event.keyCode == keyCodes.RIGHT_ARROW || event.keyCode == keyCodes.KEY_D) && !r ) {
-		rotSpeed = -config.ROTSPEED;
-		r = true;
-	}
-	
-	if ( (event.keyCode == keyCodes.LEFT_ARROW || event.keyCode == keyCodes.KEY_A) && !r ) {
-		rotSpeed = config.ROTSPEED;
-		r = true;
-	}
-}
-
-
-function stop( event ) {
-	
-	if ( (event.keyCode == keyCodes.UP_ARROW || event.keyCode == keyCodes.KEY_W) && m ) {
-		m = false;
-	}
-	
-	if ( (event.keyCode == keyCodes.DOWN_ARROW || event.keyCode == keyCodes.KEY_S) && m ) {
-		m = false;
-	}
-	
-	if ( (event.keyCode == keyCodes.RIGHT_ARROW || event.keyCode == keyCodes.KEY_D) && r ) {
-		r = false;
-	}
-	
-	if ( (event.keyCode == keyCodes.LEFT_ARROW || event.keyCode == keyCodes.KEY_A) && r ) {
-		r = false;
-	}
-}
-
-
-function setModelControls() {
-	$(window).on('keydown', move);
-	$(window).on('keyup', stop);
-}
 
 function importModelJSON () {
-	var loader = new THREE.JSONLoader();	
-	
-	var tex = THREE.ImageUtils.loadTexture('../../assets/textures/Wolf_Diffuse_256x256.jpg', null, function () {
+	var loader = new THREE.JSONLoader();
+
+	var tex = THREE.ImageUtils.loadTexture("../assets/textures/Wolf_Diffuse_256x256.jpg", null, function () {
         var mat = new THREE.MeshBasicMaterial({ map: tex, morphTargets: true });
-        loader.load('../../assets/models/Wolf.js', function (geo) {
-        	model = new THREE.Mesh(geo, mat);
+        loader.load("../assets/models/Wolf.js", function (geo) {
+            model = new THREE.Mesh(geo, mat);
             scene.add(model);
-    	});
+        });
 	});
 }
